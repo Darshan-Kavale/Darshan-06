677064fc341b10ed251ba3794c31026ec2358abd
Darshan-Kavale <darshankavale@gmail.com>
2025-07-06 09:31:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
- from flask import Flask, jsonify
-import datetime
-import random
+ FROM python:3.9-slim
 
-app = Flask(_name_)
+WORKDIR /app
 
-@app.route("/")
-def home():
-    return "<h1>🚀 DevOps Dashboard API</h1><p>Try /status to see CI/CD pipeline info.</p>"
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 
-@app.route("/status")
-def status():
-    stages = ["Build", "Test", "Deploy"]
-    results = ["Success", "Failed", "Running", "Queued"]
+COPY . .
 
-    pipeline_data = {
-        "pipeline_id": random.randint(1000, 9999),
-        "project": "DevOps-Demo",
-        "status": random.choice(results),
-        "last_run": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "stages": {
-            stage: random.choice(results) for stage in stages
-        }
-    }
-    return jsonify(pipeline_data)
+EXPOSE 5000
 
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+CMD ["python", "app.py"]
